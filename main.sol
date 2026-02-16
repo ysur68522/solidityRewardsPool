f1b70f6209a69b923976bdd6eb99c7d9b18a6b6a
ysur68522 <ysur68522@gmail.com>
2026-02-16 10:16:16 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -22,3 +22,27 @@ contract MegaMultiRewardStaking {
         function allowance(address owner, address spender) external view returns (uint256);
         function approve(address spender, uint256 amount) external returns (bool);
         function transfer(address to, uint256 amount) external returns (bool);
+        function transferFrom(address from, address to, uint256 amount) external returns (bool);
+        event Transfer(address indexed from, address indexed to, uint256 value);
+        event Approval(address indexed owner, address indexed spender, uint256 value);
+    }
+
+    /// @dev Optional EIP-2612 permit interface
+    interface IERC20Permit {
+        function permit(
+            address owner,
+            address spender,
+            uint256 value,
+            uint256 deadline,
+            uint8 v,
+            bytes32 r,
+            bytes32 s
+        ) external;
+        function nonces(address owner) external view returns (uint256);
+        function DOMAIN_SEPARATOR() external view returns (bytes32);
+    }
+
+    // -------------------------------------------------------------------------
+    // Errors
+    // -------------------------------------------------------------------------
+
